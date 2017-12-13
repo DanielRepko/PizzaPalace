b67d75ba9869af0a1ec6d7a8b6ac6f831cd167cb
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2017-12-13 16:23:05 -0500

    #16 ContactFragment * Separated view from return statement in onCreateView * fragment_contact.xml, removed default textview * Modified layout to be LinearLayout with Vertical orientation

--- app/src/main/java/com/jordan/daniel/pizzapalace/ContactFragment.java
+++ app/src/main/java/com/jordan/daniel/pizzapalace/ContactFragment.java
@@ -63,8 +63,11 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
