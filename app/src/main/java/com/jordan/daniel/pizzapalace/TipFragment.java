4aac851be4cbce4ed42919fa1c2d481a34ae8635
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2017-12-20 09:42:47 -0500

    #29 Separated view from return statement in onCreateView()

--- app/src/main/java/com/jordan/daniel/pizzapalace/TipFragment.java
+++ app/src/main/java/com/jordan/daniel/pizzapalace/TipFragment.java
@@ -63,8 +63,11 @@ public class TipFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tip, container, false);
+        View view = inflater.inflate(R.layout.fragment_tip, container, false);
+
+        
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
