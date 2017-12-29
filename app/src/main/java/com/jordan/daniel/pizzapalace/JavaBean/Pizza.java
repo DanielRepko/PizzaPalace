1a91ef01e74c940d2213e8dd97bec2ceaa1fa437
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2017-12-29 13:55:42 -0500

    #36 Added to the Pizza object      *added a private property of type string called name      *added a private property of type ArrayList<String> called toppings      *added a public contructor that takes arguments for each of the objects properties and stores the values in their respective properties

--- app/src/main/java/com/jordan/daniel/pizzapalace/JavaBean/Pizza.java
+++ app/src/main/java/com/jordan/daniel/pizzapalace/JavaBean/Pizza.java
@@ -1,9 +1,17 @@
 package com.jordan.daniel.pizzapalace.JavaBean;
 
+import java.util.ArrayList;
+
 /**
  * Created by awsom on 12/29/2017.
  */
 
 public class Pizza {
-    
+    private String name;
+    private ArrayList<String> toppings;
+
+    public Pizza(String name, ArrayList<String> toppings){
+        this.name = name;
+        this.toppings = toppings;
+    }
 }
