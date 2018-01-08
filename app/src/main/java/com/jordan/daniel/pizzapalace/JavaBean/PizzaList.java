4abf0a0321fed51f0e7a88cdcdd1eab1ffa5d5ad
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2018-01-08 11:56:21 -0500

    #68 Added code to the constructor of PizzaList *called the fillToppings() method at the start of the constructor *used the toppings ArrayList to create Pizza objects for each preset pizza *added each Pizza object into PizzaList

--- app/src/main/java/com/jordan/daniel/pizzapalace/JavaBean/PizzaList.java
+++ app/src/main/java/com/jordan/daniel/pizzapalace/JavaBean/PizzaList.java
@@ -14,7 +14,60 @@ public class PizzaList extends ArrayList {
     private ArrayList<String> toppings;
 
     public PizzaList(){
+        fillToppings();
 
+        //creating Pizza object for Meat Lover's and adding it to PizzaList
+        ArrayList<String> meatLoversPizza = new ArrayList<String>();
+        meatLoversPizza.add(this.toppings.get(0));
+        meatLoversPizza.add(this.toppings.get(1));
+        meatLoversPizza.add(this.toppings.get(2));
+        meatLoversPizza.add(this.toppings.get(4));
+        meatLoversPizza.add(this.toppings.get(11));
+        this.add(new Pizza("Meat Lover's", meatLoversPizza));
+
+        //creating Pizza object for Meat Lover's and adding it to PizzaList
+        ArrayList<String> hawaiianPizza = new ArrayList<String>();
+        hawaiianPizza.add(this.toppings.get(3));
+        hawaiianPizza.add(this.toppings.get(4));
+        this.add(new Pizza("Hawaiian", hawaiianPizza));
+
+        //creating Pizza object for Meat Lover's and adding it to PizzaList
+        ArrayList<String> motherlodePizza = new ArrayList<String>();
+        motherlodePizza.add(this.toppings.get(0));
+        motherlodePizza.add(this.toppings.get(1));
+        motherlodePizza.add(this.toppings.get(2));
+        motherlodePizza.add(this.toppings.get(3));
+        motherlodePizza.add(this.toppings.get(4));
+        motherlodePizza.add(this.toppings.get(5));
+        motherlodePizza.add(this.toppings.get(6));
+        motherlodePizza.add(this.toppings.get(7));
+        motherlodePizza.add(this.toppings.get(8));
+        motherlodePizza.add(this.toppings.get(9));
+        motherlodePizza.add(this.toppings.get(10));
+        motherlodePizza.add(this.toppings.get(11));
+        motherlodePizza.add(this.toppings.get(12));
+        motherlodePizza.add(this.toppings.get(13));
+        this.add(new Pizza("The Motherlode", motherlodePizza));
+
+        //creating Pizza object for Meat Lover's and adding it to PizzaList
+        ArrayList<String> vegetarianPizza = new ArrayList<String>();
+        vegetarianPizza.add(this.toppings.get(3));
+        vegetarianPizza.add(this.toppings.get(5));
+        vegetarianPizza.add(this.toppings.get(7));
+        vegetarianPizza.add(this.toppings.get(8));
+        vegetarianPizza.add(this.toppings.get(9));
+        vegetarianPizza.add(this.toppings.get(10));
+        vegetarianPizza.add(this.toppings.get(12));
+        this.add(new Pizza("Vegetarian", vegetarianPizza));
+
+        //creating Pizza object for Meat Lover's and adding it to PizzaList
+        ArrayList<String> cheeseburgerPizza = new ArrayList<String>();
+        cheeseburgerPizza.add(this.toppings.get(1));
+        cheeseburgerPizza.add(this.toppings.get(4));
+        cheeseburgerPizza.add(this.toppings.get(5));
+        cheeseburgerPizza.add(this.toppings.get(10));
+        cheeseburgerPizza .add(this.toppings.get(13));
+        this.add(new Pizza("Cheeseburger", cheeseburgerPizza));
     }
 
     /**
