7aeab86dad7af66c14447f3bf038ee0f0fd7e94b
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2018-01-08 11:44:26 -0500

    #68 Added a private method inside of the PizzaList object *method is called fillToppings, has a return type of void, and takes no arguments *fillToppings() fills the private property toppings with strings for all of the possible toppings

--- app/src/main/java/com/jordan/daniel/pizzapalace/JavaBean/PizzaList.java
+++ app/src/main/java/com/jordan/daniel/pizzapalace/JavaBean/PizzaList.java
@@ -2,15 +2,39 @@ package com.jordan.daniel.pizzapalace.JavaBean;
 
 import java.util.ArrayList;
 
-/**
- * Created by web on 2018-01-08.
- */
 
+/**
+ * @author Daniel Repko
+ *
+ * PizzaList extends ArrayList and is meant to be used
+ * to access all of the preset pizzas
+ */
 public class PizzaList extends ArrayList {
 
     private ArrayList<String> toppings;
 
     public PizzaList(){
 
+    }
+
+    /**
+     * This method is to be called inside of the constructor and
+     * is used to fill the toppings ArrayList
+     */
+    private void fillToppings(){
+        this.toppings.add("Pepperoni");
+        this.toppings.add("Bacon");
+        this.toppings.add("Ham");
+        this.toppings.add("Pineapple");
+        this.toppings.add("Hamburger");
+        this.toppings.add("Mushrooms");
+        this.toppings.add("Anchovies");
+        this.toppings.add("Green Olives");
+        this.toppings.add("Black Olives");
+        this.toppings.add("Peppers");
+        this.toppings.add("Onions");
+        this.toppings.add("Sausage");
+        this.toppings.add("Spinach");
+        this.toppings.add("Extra Cheese");
     }
 }
